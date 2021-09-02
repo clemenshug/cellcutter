ffdaa0bc43e22cf8bfffa7289a0526b3f20585f4
Clemens Hug <clemens.hug@gmail.com>
2021-09-01 23:45:56 -0400

    Add thread parallelism

--- cellcutter/cut.py
+++ cellcutter/cut.py
@@ -1,4 +1,6 @@
 import logging
+import concurrent.futures
+import tempfile
 from typing import Optional
 
 import numpy as np
@@ -50,6 +52,22 @@ def cut_cells(
     return cell_stack
 
 
+def cut_cells_mp(
+    image: Image,
+    cell_data: pd.DataFrame,
+    channel_idx: int,
+    window_size: int,
+    cut_array: zarr.Array,
+    mask_thumbnails: Optional[str] = None,
+) -> None:
+    img = image.get_channel(channel_idx)
+    if mask_thumbnails is not None:
+        mask_thumbnails = np.load(mask_thumbnails)
+    cell_stack = cut_cells(img, cell_data, window_size, mask_thumbnails=mask_thumbnails)
+    cut_array[channel_idx, ...] = cell_stack
+    return True
+
+
 def find_bbox_size(segmentation_mask: np.ndarray) -> int:
     props = regionprops_table(segmentation_mask, properties=["bbox"])
     return np.max(
@@ -57,20 +75,25 @@ def find_bbox_size(segmentation_mask: np.ndarray) -> int:
     )
 
 
-def save_cells_all_channels(
+def process_all_channels(
     img: Image,
     segmentation_mask: Image,
     cell_data: pd.DataFrame,
     destination: str,
     window_size: Optional[int],
     mask_cells: bool = True,
+    processes: int = 1,
 ) -> None:
     segmentation_mask_img = segmentation_mask.get_channel(0)
     # Check if all cell IDs present in the CSV file are also represented in the segmentation mask
     cell_ids_in_segmentation_mask = np.unique(segmentation_mask_img)
-    n_not_in_segmentation_mask = set(cell_data["CellID"]) - set(cell_ids_in_segmentation_mask)
+    n_not_in_segmentation_mask = set(cell_data["CellID"]) - set(
+        cell_ids_in_segmentation_mask
+    )
     if len(n_not_in_segmentation_mask) > 0:
-        raise SystemError(f"{len(n_not_in_segmentation_mask)} cell IDs in the CELL_DATA CSV file are not present in the segmentation mask.")
+        raise SystemError(
+            f"{len(n_not_in_segmentation_mask)} cell IDs in the CELL_DATA CSV file are not present in the segmentation mask."
+        )
     # Remove cells from segmentation mask that are not present in the CSV
     segmentation_mask_img[~np.isin(segmentation_mask_img, cell_data["CellID"])] = 0
     if window_size is None:
@@ -83,6 +106,7 @@ def save_cells_all_channels(
         shape=(img.n_channels, cell_data.shape[0], window_size, window_size),
         dtype=img.get_channel(0).dtype,
         compressor=Blosc(cname="zstd", clevel=3, shuffle=Blosc.BITSHUFFLE),
+        chunks=(1, 100000, None, None),
     )
     mask_thumbnails = None
     if mask_cells:
@@ -92,7 +116,7 @@ def save_cells_all_channels(
             ((window_size_h, window_size_h), (window_size_h, window_size_h)),
         )
         mask_thumbnails = np.empty(
-            (cell_data.shape[0], window_size, window_size), dtype=bool
+            (cell_data.shape[0], window_size, window_size), dtype=np.bool_
         )
         for i, c in enumerate(cell_data.itertuples()):
             centroids = np.array([c.Y_centroid, c.X_centroid]).astype(int)
@@ -103,14 +127,36 @@ def save_cells_all_channels(
                 ]
                 == c.CellID
             )
-    for i in range(img.n_channels):
-        logging.info(f"Processing channel {i}")
-        channel_img = img.get_channel(i)
-        cell_stack = cut_cells(
-            channel_img,
-            cell_data,
-            window_size,
-            mask_thumbnails=mask_thumbnails,
-        )
-        logging.info(f"Writing thumbnails for channel {i}")
-        file[i, ...] = cell_stack
+        mask_temp = tempfile.mkstemp(suffix=".npy")
+        np.save(mask_temp[1], mask_thumbnails)
+        mask_thumbnails = mask_temp[1]
+    # We can use a with statement to ensure threads are cleaned up promptly
+    with concurrent.futures.ThreadPoolExecutor(max_workers=processes) as executor:
+        # Start the load operations and mark each future with its URL
+        futures = {
+            executor.submit(
+                cut_cells_mp,
+                img,
+                cell_data,
+                i,
+                window_size,
+                file,
+                mask_thumbnails=mask_thumbnails,
+            ): i
+            for i in range(img.n_channels)
+        }
+        for future in concurrent.futures.as_completed(futures):
+            i = futures[future]
+            try:
+                data = future.result()
+                logging.info(f"Future {i} done")
+            except Exception as exc:
+                print("%r generated an exception: %s" % (i, exc))
+    # for i in range(img.n_channels):
+    #     logging.info(f"Processing channel {i}")
+    #     channel_img = img.get_channel(i)
+    #     cell_stack = cut_cells(
+    #         channel_img, cell_data, window_size, mask_thumbnails=mask_thumbnails,
+    #     )
+    #     logging.info(f"Writing thumbnails for channel {i}")
+    # file[i, ...] = cell_stack
